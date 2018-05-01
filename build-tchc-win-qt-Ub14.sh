0f97fc9fa245731dc94c729ece8b56f8a1f8a9ff
seekthex <seekthex@gmail.com>
2018-05-01 06:33:48 -0600

    fix icon copy path

--- build-tchc-win-qt-Ub14.sh
+++ build-tchc-win-qt-Ub14.sh
@@ -100,7 +100,7 @@ makewindows() {
 	cd depends
 	make HOST=x86_64-w64-mingw32
 	cd ..
-  cp /src/qt/res/icons/chaincoin.ico /src/qt/res/icons/bitcoin.ico
+  cp src/qt/res/icons/chaincoin.ico src/qt/res/icons/bitcoin.ico
 	CONFIG_SITE=$PWD/depends/x86_64-w64-mingw32/share/config.site ./configure --prefix=/ --disable-tests
 
 	sudo make
