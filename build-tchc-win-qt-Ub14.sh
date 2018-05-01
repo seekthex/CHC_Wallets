11f9b8b952de5ef8e76e780e2bf5e4649bdc1a3d
seekthex <seekthex@gmail.com>
2018-04-30 21:07:18 -0600

    make .sh executable

--- build-tchc-win-qt-Ub14.sh
+++ build-tchc-win-qt-Ub14.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-#Version 0.0.0.1
+#Version 0.0.0.s
 #Info: Installs Chaincoind daemon
-#Chaincoin Testnet Version 0.16.x 
-#Testing OS: Ubuntu 14.04 
+#Chaincoin Testnet Version 0.16.x
+#Testing OS: Ubuntu 14.04
 #TODO: not testested
 #TODO: massive cleanup
 
@@ -19,7 +19,7 @@ message() {
 installgeneraldependencies(){
 
 	message "installing dependencies"
-	
+
 	# General
 	sudo apt update -y
 	sudo apt-get install build-essential libtool autotools-dev automake pkg-config libssl-dev libevent-dev bsdmainutils python3 git -y
@@ -35,10 +35,10 @@ installgeneraldependencies(){
 
 	# upnc - Optional (see --with-miniupnpc and --enable-upnp-default):
 	sudo apt-get install libminiupnpc-dev -y
-	
+
 	# zero message queue
 	sudo apt-get install libzmq3-dev -y
-	
+
 	# QT5 - QT Wallet
 	sudo apt-get install libqt5gui5 libqt5core5a libqt5dbus5 qttools5-dev qttools5-dev-tools libprotobuf-dev protobuf-compiler -y
 
@@ -48,7 +48,7 @@ installgeneraldependencies(){
 
 
 
-installmorestuff() {	
+installmorestuff() {
 	message "installing more stuff"
 	# General dependencies
 	sudo apt update -y
@@ -64,14 +64,14 @@ makechaincoin() {
 	git clone https://github.com/ChainCoin/ChainCoin.git -b Chaincoin_0.16-dev
 	cd ChainCoin
 	make clean
-	
+
 	./autogen.sh
-	
+
 	sudo ./contrib/install_db4.sh berkeley48
 	export BDB_PREFIX='/db4'
 
 	./configure CPPFLAGS="-I${BDB_PREFIX}/include/ -O2 -fPIC" LDFLAGS="-L${BDB_PREFIX}/lib/" --disable-tests
-} 
+}
 
 makewindows() {
 
@@ -82,9 +82,9 @@ makewindows() {
 	cd ..
 
 	CONFIG_SITE=$PWD/depends/x86_64-w64-mingw32/share/config.site ./configure --prefix=/ --disable-tests
-	
+
 	sudo make
-	
+
 	sudo make install
 }
 
