5a130db6d4f7ca497191a2017e19726b584ac926
seekthex <seekthex@gmail.com>
2018-05-01 19:15:56 -0600

    condence script

--- build-tchc-win-qt-Ub14.sh
+++ build-tchc-win-qt-Ub14.sh
@@ -9,19 +9,20 @@
 message() {
 
 	echo "╒════════════════════════════════════════════════════════>>>"
-	echo "| $1" BAAAHHH!!!
+	echo "| $1"
 	echo "╘════════════════════════════════════════════════════════<<<"
 }
-
-
 
 
 installgeneraldependencies(){
 
 	message "installing dependencies"
 
-	# General
+	# General dependencies
 	sudo apt update -y
+	sudo apt upgrade -y
+	sudo apt-get install git -y
+	sudo apt install curl -y
 	sudo apt-get install build-essential -y
 	sudo apt-get install libtool -y
 	sudo apt-get install autotools-dev -y
@@ -30,8 +31,8 @@ installgeneraldependencies(){
 	sudo apt-get install libssl-dev -y
 	sudo apt-get install libevent-dev -y
 	sudo apt-get install bsdmainutils -y
-  sudo apt-get install git -y
 	sudo apt-get install python3 -y
+
 
 	# Boost C macros - Bitcoin core trying to remove this
 	sudo apt-get install libboost-filesystem-dev -y
@@ -54,74 +55,50 @@ installgeneraldependencies(){
 	sudo apt-get install libzmq3-dev -y
 
 	# QT5 - QT Wallet
-	sudo apt-get install libqt5gui5 libqt5core5a libqt5dbus5 qttools5-dev qttools5-dev-tools libprotobuf-dev protobuf-compiler -y
-
+	sudo apt-get install libqt5gui5
+	sudo apt-get install libqt5core5a
+	sudo apt-get install libqt5dbus5
+	sudo apt-get install qttools5-dev
+	sudo apt-get install qttools5-dev-tools
+	sudo apt-get install libprotobuf-dev
+	sudo apt-get install protobuf-compiler -y
 	# QR
 	sudo apt-get install libqrencode-dev -y
-}
-
-
-
-installmorestuff() {
-	message "installing more stuff"
-	# General dependencies
-	sudo apt update -y
-	sudo apt upgrade -y
-  sudo apt install build-essential -y #duplicate
-	sudo apt install libtool -y #duplicate
-	sudo apt install autotools-dev -y #duplicate
-	sudo apt install automake -y #duplicate
-	sudo apt install pkg-config -y #think I forgot this
-	sudo apt install bsdmainutils  git -y
-	sudo apt install curl -y #do I need this
-	sudo apt install git -y #should alreay have this
 	# cross compilation toolchain
 	sudo apt install g++-mingw-w64-x86-64 -y
 }
 
+
+
 makechaincoin() {
-	message "preparing the the chaincoin..."
+	message "preparing the chaincoin..."
 	git clone https://github.com/ChainCoin/ChainCoin.git -b Chaincoin_0.16-dev
 	cd ChainCoin
 	make clean
-
 	./autogen.sh
-
 	sudo ./contrib/install_db4.sh berkeley48
 	export BDB_PREFIX='/db4'
-
 	./configure CPPFLAGS="-I${BDB_PREFIX}/include/ -O2 -fPIC" LDFLAGS="-L${BDB_PREFIX}/lib/" --disable-tests
 }
 
-makewindows() {
-
+makechaincoinqt() {
 	# build steps
 	PATH=$(echo "$PATH" | sed -e 's/:\/mnt.*//g') # strip out problematic Windows %PATH% imported var
 	cd depends
 	make HOST=x86_64-w64-mingw32
 	cd ..
-  cp src/qt/res/icons/chaincoin.ico src/qt/res/icons/bitcoin.ico
+  cp /src/qt/res/icons/chaincoin.ico /src/qt/res/icons/bitcoin.ico
 	CONFIG_SITE=$PWD/depends/x86_64-w64-mingw32/share/config.site ./configure --prefix=/ --disable-tests
-
 	sudo make
-
 	sudo make install
 }
 
 
-success() {
-
-	message "SUCCESS you ran some code, feel safe, be happy"
-}
-
 
 install() {
-  cd ~
 	installgeneraldependencies
 	makechaincoin
-	installmorestuff
-	makewindows
-	success
+	makechaincoinqt
 }
 
 #main
