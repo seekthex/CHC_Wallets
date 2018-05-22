f2e1accb2f61f42930be8549165a0d52d629079c
J B <37249474+seekthex@users.noreply.github.com>
2018-05-22 18:54:05 -0400

    Update build-chc-9.3.3-ub16.sh

--- sh/build-chc-9.3.3-ub16.sh
+++ sh/build-chc-9.3.3-ub16.sh
@@ -19,17 +19,17 @@ build_chc_wallet() {
 	message "Installing pre-dependencies..."
 
 	sudo apt-get update
-	sudo apt-get install automake
-	sudo apt-get install libdb++-dev
-	sudo apt-get install build-essential libtool autotools-dev
-	sudo apt-get install autoconf pkg-config libssl-dev
-	sudo apt-get install libboost-all-dev
-	sudo apt-get install libminiupnpc-dev
-	sudo apt-get install git
-	sudo apt-get install software-properties-common
-	sudo apt-get install python-software-properties
-	sudo apt-get install g++ 
-	sudo add-apt-repository ppa:bitcoin/bitcoin
+	sudo apt-get install automake -y
+	sudo apt-get install libdb++-dev -y
+	sudo apt-get install build-essential libtool autotools-dev -y
+	sudo apt-get install autoconf pkg-config libssl-dev -y
+	sudo apt-get install libboost-all-dev -y
+	sudo apt-get install libminiupnpc-dev -y
+	sudo apt-get install git -y
+	sudo apt-get install software-properties-common -y
+	sudo apt-get install python-software-properties -y
+	sudo apt-get install g++ -y
+	sudo add-apt-repository ppa:bitcoin/bitcoin -y
 	sudo apt-get update
 	sudo apt-get install libdb4.8-dev libdb4.8++-dev -y 
 
