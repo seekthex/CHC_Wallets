ede1c7228636bcf00de8e71b42c78c261e7b8fbd
J B <37249474+seekthex@users.noreply.github.com>
2018-06-03 13:45:04 -0600

    Update build-tchc-ub16.sh

--- sh/build-tchc-ub16.sh
+++ sh/build-tchc-ub16.sh
@@ -66,10 +66,11 @@ build_chc_wallet() {
 	echo "server=1" >> chaincoin.conf
 	echo "debug=1" >> chaincoin.conf
 	echo "prematurewitness=1" >> chaincoin.conf
-	echo "addnode=78.47.108.196:11996" >> chaincoin.conf
 
-#echo "addresstype=p2sh-segwit" >> chaincoin.conf
-#echo "changetype=p2sh-segwit" >> chaincoin.conf
+
+echo "addresstype=p2sh-segwit" >> chaincoin.conf
+echo "changetype=p2sh-segwit" >> chaincoin.conf
+
 	echo "rpcuser=123" >> chaincoin.conf
 	echo "rpcpassword=1234" >> chaincoin.conf
 	echo "rpcport=11995" >> chaincoin.conf
