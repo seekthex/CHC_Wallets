962e6d1307dd2d85a424a2587eb7daaf4e488fb6
seekthex <seekthex@gmail.com>
2018-06-30 17:45:58 -0600

    masternode=1 test

--- mainnet/ubuntu/sh/test.sh
+++ mainnet/ubuntu/sh/test.sh
@@ -1,5 +1,19 @@
+michel ~/test> cat leaptest.sh
 #!/bin/bash
-# Ask the user for their name
-echo Hello, who am I talking to?
-read varname
-echo It\'s nice to meet you $varname
+# This script will test if you have given a leap year or not.
+
+echo "Type the year that you want to check (4 digits), followed by [ENTER]:"
+
+read year
+
+if (( ("$year" % 400) == "0" )) || (( ("$year" % 4 == "0") && ("$year" % 100 !=
+"0") )); then
+  echo "$year is a leap year."
+else
+  echo "This is not a leap year."
+fi
+
+michel ~/test> leaptest.sh
+Type the year that you want to check (4 digits), followed by [ENTER]:
+2000
+2000 is a leap year.
