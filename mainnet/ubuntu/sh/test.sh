25ae4ed0770e3f64b1b82be7908e9c3fc8da3c53
seekthex <seekthex@gmail.com>
2018-06-30 17:42:59 -0600

    masternode=1 test

--- mainnet/ubuntu/sh/test.sh
+++ mainnet/ubuntu/sh/test.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
-in=/autofs/cluster/transcend/mega/scripts/
-insub=/autofs/cluster/transcend/mega/dtn/20141015/123
-out=/autofs/cluster/transcend/mega/scripts/1256
-
-for i in $(cat $in/list_subjects.txt); do
-echo "$i"
-mkdir ${insub}/${i}/file.nii.gz -k ${insub}/${i}/rh.nii.gz -M
-
-wait
-done
+# Ask the user for their name
+echo Hello, who am I talking to?
+read varname
+echo It\'s nice to meet you $varname
