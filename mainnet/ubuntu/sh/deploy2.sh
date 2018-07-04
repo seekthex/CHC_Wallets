605758166c3a6d522868d82325afef29e9cfdea4
seekthex <seekthex@gmail.com>
2018-07-04 12:56:37 -0600

    edits to deploy2.sh

--- mainnet/ubuntu/sh/deploy2.sh
+++ mainnet/ubuntu/sh/deploy2.sh
@@ -93,11 +93,13 @@ makechaincoinqt() {
 
 	PATH=$(echo "$PATH" | sed -e 's/:\/mnt.*//g') # strip out problematic Windows %PATH% imported var
 	cd depends
-	make HOST=i686-w64-mingw32
+	make HOST=x86_64-w64-mingw32
 	cd ..
-	./autogen.sh # not required when building from tarball
-	CONFIG_SITE=$PWD/depends/i686-w64-mingw32/share/config.site ./configure --prefix=/
+	CONFIG_SITE=$PWD/depends/x86_64-w64-mingw32/share/config.site ./configure --prefix=/
 	sudo make
+	sudo make install
+
+
 }
 
 install() {
